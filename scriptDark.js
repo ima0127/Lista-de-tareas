5286acac35bc089a571060995ad687e4068346ef
Ismael Peralta <ismaelperalta2002@gmail.com>
2022-04-25 19:15:59 -0400

    arreglando responsive

---
+++ scriptDark.js
@@ -0,0 +1,15 @@
+const btnDark = document.querySelector(".modeDark").addEventListener("click",function(){
+    const darkness = document.body.classList.toggle("dark");
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('darkMode', 'enabled');
+    }
+    else{
+        localStorage.setItem('darkMode', 'disabled');
+        localStorage.removeItem('darkMode');
+        localStorage.clear()
+    }
+});
+
+if(localStorage.getItem('darkMode') == 'enabled'){
+    document.body.classList.toggle('dark');
+}
