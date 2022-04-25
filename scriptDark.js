d78499873d2113ffc1a33eb99fc4a4d960ce31cc
Ismael Peralta <76632427+ima0127@users.noreply.github.com>
2022-04-25 19:22:14 -0400

    Update scriptDark.js

--- scriptDark.js
+++ scriptDark.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const btnDark = document.querySelector(".modeDark").addEventListener("click",function(){
     const darkness = document.body.classList.toggle("dark");
     if(document.body.classList.contains('dark')){
@@ -30,4 +30,3 @@ const btnDark = document.querySelector(".modeDark").addEventListener("click",fun
 if(localStorage.getItem('darkMode') == 'enabled'){
     document.body.classList.toggle('dark');
 }
->>>>>>> 0485df7f17fa9ac96f35000c5b6e0ad6ea4b3775
