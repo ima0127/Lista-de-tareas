2315cab18e2bdb3bc8863eb375f63aa06d707914
Ismael Peralta <ismaelperalta2002@gmail.com>
2022-04-27 10:21:53 -0400

    arreglado problema de Darkmode

--- scriptDark.js
+++ scriptDark.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const btnDark = document.querySelector(".modeDark").addEventListener("click",function(){
     const darkness = document.body.classList.toggle("dark");
     if(document.body.classList.contains('dark')){
@@ -14,20 +14,3 @@ const btnDark = document.querySelector(".modeDark").addEventListener("click",fun
 if(localStorage.getItem('darkMode') == 'enabled'){
     document.body.classList.toggle('dark');
 }
-=======
-const btnDark = document.querySelector(".modeDark").addEventListener("click",function(){
-    const darkness = document.body.classList.toggle("dark");
-    if(document.body.classList.contains('dark')){
-        localStorage.setItem('darkMode', 'enabled');
-    }
-    else{
-        localStorage.setItem('darkMode', 'disabled');
-        localStorage.removeItem('darkMode');
-        localStorage.clear()
-    }
-});
-
-if(localStorage.getItem('darkMode') == 'enabled'){
-    document.body.classList.toggle('dark');
-}
->>>>>>> 0485df7f17fa9ac96f35000c5b6e0ad6ea4b3775
